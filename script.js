94c337152ed7a2e66a7df5ace3d8337926f97560
Rakesh Munukuntla <130857014+Rakeshmunukuntla@users.noreply.github.com>
2024-11-21 12:19:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-function stringChop(str, size) {
-  // your code here
+function stringChop(str, size)
+	{
+  // your code here.
+		let arr=[];
+		let ch="";
+		let count=0;
+		for (let index = 0; index < str.length; index++) 
+		{
+			ch+=str[index];
+			count++;
+			if(count==size)
+			{
+				count=0;
+				arr.push(ch);
+				ch="";
+				
+			}
+		}
+		arr.push(ch);
+		return arr;
 }
+
 
 // Do not change the code below
 const str = prompt("Enter String.");
